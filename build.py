1d391d41055cabd97e20e60301fd606736c18654
Jacques Supcik <jacques@supcik.net>
2025-07-28 21:41:39 +0200

    Refactor build process and update web installer UI

--- build.py
+++ build.py
@@ -1,11 +1,32 @@
+from dataclasses import dataclass, field
 from pathlib import Path
 from shutil import copyfile
-from subprocess import run
 
 from jinja2 import Environment, FileSystemLoader
 
+
+@dataclass
+class Card:
+    name: str
+    stem: str
+    manifest: str
+    variables: dict = field(default_factory=dict)
+
+
+artwork = Card(name="Oxocard Mini Artwork", manifest="artwork", stem="artwork")
+galaxy = Card(name="Oxocard Mini Galaxy", manifest="galaxy", stem="galaxy")
+science = Card(name="Oxocard Mini Science", manifest="science", stem="science")
+connect = Card(name="Oxocard Mini Connect", manifest="connect", stem="connect")
+connect_makey = Card(
+    name="Oxocard Mini Connect (Makey Edition)",
+    manifest="connect-makey",
+    stem="connect",
+    variables={"is_makey": True},
+)
+
 firmware_path = Path("oxocard_binaries")
-cards = ["artwork", "galaxy", "science", "connect"]
+
+cards = [artwork, galaxy, science, connect, connect_makey]
 
 
 def main():
@@ -14,8 +35,8 @@ def main():
     )
 
     products = []
-    (Path("webpage") / "firmware" / "merged").mkdir(parents=True, exist_ok=True)
     (Path("webpage") / "firmware" / "common").mkdir(parents=True, exist_ok=True)
+    (Path("webpage") / "firmware" / "nvs").mkdir(parents=True, exist_ok=True)
 
     copyfile(
         Path("oxocard_binaries") / "common" / "bootloader.bin",
@@ -28,23 +49,10 @@ def main():
     )
 
     for card in cards:
-        firmware = sorted(list((firmware_path / card).glob("*.bin")), reverse=True)[0]
+        firmware = sorted(
+            list((firmware_path / card.stem).glob("*.bin")), reverse=True
+        )[0]
         version = firmware.stem.split("_")[-1]
-        name = " ".join([i.title() for i in firmware.stem.split("_")[:-1]])
-        # fmt: off
-        cmd = [
-            "esptool.py",
-            "--chip", "esp32", "merge_bin",
-            "-o", str(Path("webpage") / "firmware" / "merged" / firmware.name),
-            "--flash_mode", "dio",
-            "--flash_freq", "80m",
-            "--flash_size", "8MB",
-            "0x1000", "oxocard_binaries/common/bootloader.bin",
-            "0x8000", "oxocard_binaries/common/partition-table.bin",
-            "0x10000", str(firmware),
-        ]
-        # fmt: on
-        run(cmd)
 
         copyfile(
             firmware,
@@ -52,19 +60,27 @@ def main():
         )
 
         template = env.get_template("manifest.json")
-        with open(
-            Path("webpage") / f"manifest_{firmware.with_suffix('.json').name}", "w"
-        ) as f:
+        with open(Path("webpage") / f"manifest_{card.manifest}.json", "w") as f:
             f.write(
-                template.render(name=name, version=version, file_name=firmware.name)
+                template.render(
+                    name=card.name,
+                    version=version,
+                    file_name=firmware.name,
+                    **card.variables,
+                )
             )
-        products.append({"name": name, "stem": firmware.stem, "version": version})
+
+        products.append({"name": card.name, "stem": card.manifest, "version": version})
 
         template = env.get_template("index.html")
         with open(Path("webpage") / "index.html", "w") as f:
             f.write(template.render(products=products))
 
+    copyfile(
+        Path("nvs") / "makey.bin",
+        Path("webpage") / "firmware" / "nvs" / "nvs-makey.bin",
+    )
+
 
 if __name__ == "__main__":
-    main()
     main()
